7d8139bbdd78f44f10f82a72380d8f745487a56d
Dante3887 <dman8723@gmail.com>
2022-08-27 17:10:45 +0800

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width , initial-scale=1">
+    <title>Dante Profile</title>
+
+    <link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav class="navBar">
+            <a class="navBar-item" href="">Home</a>
+            <a class="navBar-item" href="about">About Me</a>
+            <a class="navBar-item" href="">My Work</a>
+        </nav>
+    </header>
+
+    <main class="intro">
+        <div class="intro-topic">Welcome to <span class="webName">DanteSite</span></div>
+        <div class="intro-content-1">I am developing a webist to record my project.</div>
+        <div class="intro-content-2"></div>
+    </main>
+
+
+</body>
